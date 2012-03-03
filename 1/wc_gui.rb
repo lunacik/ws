c04643494807da8c66360c4281af97a003866e49
lunacik <lunacik1@yahoo.pl>
2012-03-03 21:55:33 +0200

    my commit

---
+++ 1/wc_gui.rb
@@ -0,0 +1,113 @@
+
+require 'gtk2'
+require './country_names'
+require './weather_client'
+
+
+class RubyApp < Gtk::Window
+    def initialize
+        super
+        set_title "Weather Client"
+        signal_connect "destroy" do 
+            Gtk.main_quit 
+        end
+        
+        @cloud_image = Gtk::Image.new "cloud.png"
+        @sun_image = Gtk::Image.new "sun.png"
+        @cry_image = Gtk::Image.new "cry.png"
+        @smile_image = Gtk::Image.new "smile.png"
+        init_ui
+
+        set_default_size 250, 400
+        set_window_position Gtk::Window::POS_CENTER
+        @weatherClient = WeatherClient.new
+        show_all
+
+        @cloud_image.visible = false
+        @sun_image.visible = false
+        @cry_image.visible = false
+    end
+    
+    def init_ui
+        fixed = Gtk::Fixed.new
+
+        cb1 = Gtk::ComboBox.new
+            cb1.signal_connect "changed" do |w|
+                on_changed1(w)
+        end
+        
+        CONTINENTS.keys.each {|cont| cb1.append_text cont}
+        
+        @cb2 = Gtk::ComboBox.new
+            @cb2.signal_connect "changed" do |w|
+                on_changed2(w)
+        end
+
+        @cb3 = Gtk::ComboBox.new 
+            @cb3.signal_connect "changed" do |w|
+                on_changed3(w)
+        end
+        
+        cb1.set_width_request 200
+        @cb2.set_width_request 200
+        @cb3.set_width_request 200
+
+
+        fixed.put cb1, 30, 30
+
+        fixed.put @cb2, 30, 53
+        
+        fixed.put @cb3, 30, 76
+        @weatherLabel = Gtk::Label.new
+
+        fixed.put @weatherLabel, 0, 150        
+        @weatherLabel.set_text "Choose some location"
+        fixed.put @cloud_image, 50, 350
+        fixed.put @sun_image, 50, 350
+        fixed.put @cry_image, 50, 350
+        fixed.put @smile_image, 50, 350
+        add fixed
+    end
+
+    def on_changed1 sender
+        @cb2.model = Gtk::ListStore.new String
+        @cb3.model = Gtk::ListStore.new String
+        countries = CONTINENTS[sender.active_text]
+        countries.each {|country| @cb2.append_text country}
+    end
+
+    
+    def on_changed2 sender
+        @cb3.model = Gtk::ListStore.new String
+        @country = sender.active_text
+        cities = @weatherClient.getCities @country
+        cities.each {|city| @cb3.append_text city}
+    end
+
+
+    def on_changed3 sender
+        @weatherLabel.set_text @weatherClient.getWeather sender.active_text, @country
+        @smile_image.visible = false
+        if @weatherLabel.text == "Data Not Found"
+            @sun_image.visible = false
+            @cloud_image.visible = false
+            @cry_image.visible = true
+        elsif @weatherLabel.text.include? "cloud" or @weatherLabel.text.include? "overcast"
+            @sun_image.visible = false
+            @cry_image.visible = false
+            @cloud_image.visible = true
+        else
+            @cloud_image.visible = false
+            @cry_image.visible = false
+            @sun_image.visible = true
+        end
+    end
+end
+
+
+
+Gtk.init
+    window = RubyApp.new
+Gtk.main
+
+
---
+++ 1/weather_client.rb
@@ -0,0 +1,32 @@
+
+require 'savon'
+require 'rexml/document'
+include REXML
+
+class WeatherClient
+    def initialize
+        @client = Savon::Client.new "http://webservicex.com/globalweather.asmx?WSDL"
+    end
+
+    def getWeather cityName, countryName
+        response = @client.request :get_weather, 
+                body: {"CityName" => cityName, "CountryName" => countryName}
+        xml = response.to_hash[:get_weather_response][:get_weather_result].to_s
+        return xml if xml == "Data Not Found"
+        xml["utf-16"] = "utf-8" 
+        xmldoc = Document.new(xml)
+        weatherInfo = ""
+        xmldoc.elements.each("CurrentWeather/*") {|e| weatherInfo += e.name + " " + e.text + "\n"}
+        weatherInfo
+    end
+
+    def getCities countryName
+        response = @client.request :get_cities_by_country, body: {"CountryName" => countryName}
+        xml = response.to_hash[:get_cities_by_country_response][:get_cities_by_country_result]
+        xmldoc = REXML::Document.new(xml.to_s)
+        cities = []     
+        xmldoc.elements.each("NewDataSet/Table/City") {|city| cities.push city.text} 
+        cities
+    end
+end
+
